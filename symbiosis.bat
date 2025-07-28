532dd3b2f47d35ad266368081b684bf1bc95dad0
Wings <wingstransferofficial@gmail.com>
2025-07-28 16:02:36 +0530

    frontend bat file

---
+++ symbiosis.bat
@@ -0,0 +1,6 @@
+@echo off
+REM ─────────── Change this to your project folder ───────────
+cd /d "D:\projects OOPS\Symbiosis Management system\frontend"
+npm start
+REM ─────────── Keeps window open on exit ───────────
+pause
